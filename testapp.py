db0d502323b2fad7a90f48902ef3e3365b90f8e9
NirukoX <ivanaalpeche@gmail.com>
2025-03-01 19:18:57 +0800

    Testing

--- testapp.py
+++ testapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "App is running!"
+    return "App is currently running!"
 
 if __name__ == '__main__':
     from threading import Timer
